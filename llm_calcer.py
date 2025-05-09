0b567c12be0bc24290b01ec6e7259a8c6a99c48b
Tao Song <tasong@nvidia.com>
2025-05-09 15:43:17 +0800

    update readme and init repo

---
+++ llm_calcer.py
@@ -0,0 +1,268 @@
+import csv
+import os
+import json
+import math
+import re
+from pathlib import Path
+from huggingface_hub import hf_hub_download, list_repo_files
+
+
+def axwy_to_bytes(axwy: str):
+    assert axwy in ["a16w4", "a16w8", "a8w8", "a8w4", "a4w4"], f"Invalid axwy: {axwy}"
+    ab, wb = re.match(r"a(\d+)w(\d+)", axwy).groups()
+    return float(ab) / 8, float(wb) / 8
+
+
+def get_model_config(hf_repo: str):
+
+    weight_exts = ["bin", "safetensors", "pt", "pth", "ckpt", "npz"]
+
+    local_path = Path("models/" + hf_repo.split("/")[-1])
+    if not os.path.exists(local_path):
+        os.makedirs(local_path, exist_ok=True)
+        files = list_repo_files(hf_repo)
+        for f in files:
+            if not any(f.endswith(ext) for ext in weight_exts):
+                hf_hub_download(repo_id=hf_repo, filename=f, local_dir=local_path, local_dir_use_symlinks=False)
+
+    try:
+        conf_path = os.path.join(local_path, "config.json")
+        with open(conf_path, "r") as f:
+            config = json.load(f)
+    except FileNotFoundError:
+        raise
+
+    return config
+
+
+class llama:
+    def __init__(self, hf_repo: str, custom_config: dict = None):
+        self.hf_repo = hf_repo
+        self.config = get_model_config(hf_repo)
+        if custom_config is not None:
+            self.config.update(custom_config)
+        self.num_layers = self.config["num_hidden_layers"]
+        self.hidden_size = self.config["hidden_size"]
+        self.num_heads = self.config["num_attention_heads"]
+        self.num_kv_heads = self.config["num_key_value_heads"] if "num_key_value_heads" in self.config else self.num_heads
+        self.head_dim = self.config["head_dim"] if "head_dim" in self.config else self.hidden_size // self.num_heads
+        self.intermediate_size = self.config["intermediate_size"]
+        self.vocab_size = self.config["vocab_size"]
+        self.max_position_embeddings = self.config["max_position_embeddings"]
+
+    def calc_inference_tops(self, tokens: int, past_tokens: int = 0, batch: int = 1):
+        embedding_macs = self.vocab_size * self.hidden_size * tokens
+        lm_head_macs = self.hidden_size * self.vocab_size * tokens
+
+        q_proj_macs = self.hidden_size * self.num_heads * self.head_dim * tokens
+        k_proj_macs = self.hidden_size * self.num_kv_heads * self.head_dim * tokens
+        v_proj_macs = self.hidden_size * self.num_kv_heads * self.head_dim * tokens
+        out_proj_macs = self.hidden_size * self.num_heads * self.head_dim * tokens
+
+        attention_tokens = tokens + past_tokens
+        attention_qk_macs = self.num_heads * attention_tokens * self.head_dim * attention_tokens
+        attention_softmax_macs = self.num_heads * attention_tokens * attention_tokens
+        attention_qkv_macs = self.num_heads * attention_tokens * attention_tokens * self.head_dim
+
+        mlp_ffn_macs = self.intermediate_size * self.hidden_size * tokens
+        mlp_matdot_macs = self.intermediate_size * tokens
+
+        transformer_block_macs = (
+            q_proj_macs
+            + k_proj_macs
+            + v_proj_macs
+            + out_proj_macs
+            + attention_qk_macs
+            + attention_softmax_macs
+            + attention_qkv_macs
+            + mlp_ffn_macs * 3
+            + mlp_matdot_macs
+        )
+
+        model_total_macs = transformer_block_macs * self.num_layers + embedding_macs + lm_head_macs
+        model_total_macs = model_total_macs * batch
+        return model_total_macs * 2 / 1e12
+
+    def calc_inference_dram_gbs(self, tokens: int, past_tokens: int = 0, batch: int = 1, axwy: str = "a16w4"):
+        embedding_params = self.vocab_size * self.hidden_size
+        lm_head_params = self.hidden_size * self.vocab_size
+
+        q_proj_params = self.hidden_size * self.num_heads * self.head_dim
+        k_proj_params = self.hidden_size * self.num_kv_heads * self.head_dim
+        v_proj_params = self.hidden_size * self.num_kv_heads * self.head_dim
+        out_proj_params = self.hidden_size * self.num_heads * self.head_dim
+
+        mlp_ffn_params = self.intermediate_size * self.hidden_size
+
+        attention_tokens = tokens + past_tokens
+        q_activations = tokens * self.num_heads * self.head_dim
+        k_activations = attention_tokens + self.num_kv_heads * self.head_dim
+        v_activations = attention_tokens + self.num_kv_heads * self.head_dim
+
+        transformer_block_params = q_proj_params + k_proj_params + v_proj_params + out_proj_params + mlp_ffn_params * 3
+
+        total_params = embedding_params + lm_head_params + transformer_block_params * self.num_layers
+        total_activations = (q_activations + k_activations + v_activations) * batch
+
+        ab, wb = axwy_to_bytes(axwy)
+        total_bytes = total_params * wb + total_activations * ab
+        return total_bytes / 1e9
+
+
+class llama4:
+    def __init__(self, hf_repo: str, custom_config: dict = None):
+        """llama4 config contains text_config and vision_config."""
+        self.hf_repo = hf_repo
+        self.config = get_model_config(hf_repo)["text_config"]
+        if custom_config is not None:
+            self.config.update(custom_config)
+        self.num_layers = self.config["num_hidden_layers"]
+        self.hidden_size = self.config["hidden_size"]
+        self.num_heads = self.config["num_attention_heads"]
+        self.num_kv_heads = self.config["num_key_value_heads"] if "num_key_value_heads" in self.config else self.num_heads
+        self.head_dim = self.config["head_dim"] if "head_dim" in self.config else self.hidden_size // self.num_heads
+        self.num_experts_per_tok = self.config["num_experts_per_tok"]
+        self.num_experts = self.config["num_local_experts"]
+        self.intermediate_size = self.config["intermediate_size"]
+        self.intermediate_size_mlp = self.config["intermediate_size_mlp"]
+        self.vocab_size = self.config["vocab_size"]
+        self.max_position_embeddings = self.config["max_position_embeddings"]
+        self.attn_temperature_tuning = self.config["attn_temperature_tuning"]
+
+        interleave_moe_layer_step = self.config["interleave_moe_layer_step"]
+        no_rope_step = 4  # FIXME hyper param = attn_temperature_tuning
+        self.moe_layers = math.floor(self.num_layers / interleave_moe_layer_step)
+        self.no_rope_layers = math.floor(self.num_layers / no_rope_step)
+        assert self.moe_layers == len(self.config["moe_layers"])
+        assert self.no_rope_layers == self.config["no_rope_layers"].count(0)
+
+    def calc_inference_tops(self, tokens: int, past_tokens: int = 0, batch: int = 1):
+        embedding_macs = self.vocab_size * self.hidden_size * tokens
+        lm_head_macs = self.hidden_size * self.vocab_size * tokens
+
+        q_proj_macs = self.hidden_size * self.num_heads * self.head_dim * tokens
+        k_proj_macs = self.hidden_size * self.num_kv_heads * self.head_dim * tokens
+        v_proj_macs = self.hidden_size * self.num_kv_heads * self.head_dim * tokens
+        out_proj_macs = self.hidden_size * self.num_heads * self.head_dim * tokens
+
+        attention_tokens = tokens + past_tokens
+        attn_scales_macs = self.num_heads * self.head_dim * attention_tokens
+        attention_qk_macs = self.num_heads * attention_tokens * self.head_dim * attention_tokens
+        attention_softmax_macs = self.num_heads * attention_tokens * attention_tokens
+        attention_qkv_macs = self.num_heads * attention_tokens * attention_tokens * self.head_dim
+
+        mlp_ffn_macs = self.intermediate_size_mlp * self.hidden_size * tokens
+        mlp_matdot_macs = self.intermediate_size_mlp * tokens
+
+        moe_router_macs = self.num_experts * self.hidden_size * tokens
+        moe_r_experts_ffn_macs = self.intermediate_size * self.hidden_size * self.num_experts_per_tok * tokens
+        moe_r_experts_matdot_macs = self.intermediate_size * self.num_experts_per_tok * tokens
+        moe_s_expert_ffn_macs = self.intermediate_size * self.hidden_size * tokens
+        moe_s_expert_matdot_macs = self.intermediate_size * tokens
+
+        layer_attention_macs = (
+            q_proj_macs
+            + k_proj_macs
+            + v_proj_macs
+            + out_proj_macs
+            + attention_qk_macs
+            + attention_softmax_macs
+            + attention_qkv_macs
+        )
+        layer_moe_macs = (
+            moe_router_macs
+            + moe_r_experts_ffn_macs * 3
+            + moe_r_experts_matdot_macs
+            + moe_s_expert_ffn_macs * 3
+            + moe_s_expert_matdot_macs
+        )
+        layer_mlp_macs = mlp_ffn_macs * 3 + mlp_matdot_macs
+        model_total_macs = (
+            layer_attention_macs * self.num_layers
+            + attn_scales_macs * self.no_rope_layers
+            + layer_moe_macs * self.moe_layers
+            + layer_mlp_macs * (self.num_layers - self.moe_layers)
+            + embedding_macs
+            + lm_head_macs
+        )
+        model_total_macs = model_total_macs * batch
+        return model_total_macs * 2 / 1e12
+
+    def calc_inference_dram_gbs(self, tokens: int, past_tokens: int = 0, batch: int = 1, axwy: str = "a16w4"):
+        embedding_params = self.vocab_size * self.hidden_size
+        lm_head_params = self.hidden_size * self.vocab_size
+
+        q_proj_params = self.hidden_size * self.num_heads * self.head_dim
+        k_proj_params = self.hidden_size * self.num_kv_heads * self.head_dim
+        v_proj_params = self.hidden_size * self.num_kv_heads * self.head_dim
+        out_proj_params = self.hidden_size * self.num_heads * self.head_dim
+
+        mlp_ffn_params = self.intermediate_size_mlp * self.hidden_size
+        moe_ffn_params = self.intermediate_size * self.hidden_size
+        moe_router_params = self.hidden_size * self.num_experts
+
+        attention_tokens = tokens + past_tokens
+        q_activations = self.num_heads * tokens * self.head_dim
+        k_activations = attention_tokens + self.num_kv_heads * self.head_dim
+        v_activations = attention_tokens + self.num_kv_heads * self.head_dim
+
+        layer_attention_params = q_proj_params + k_proj_params + v_proj_params + out_proj_params
+        layer_mlp_params = mlp_ffn_params * 3
+
+        # Assume load all experts in the prefill stage and only activated experts in the decode stage
+        activated_experts = (self.num_experts if tokens > 1 else self.num_experts_per_tok) + 1
+        layer_moe_params = moe_router_params + moe_ffn_params * 3 * activated_experts
+
+        total_params = (
+            embedding_params
+            + lm_head_params
+            + layer_attention_params * self.num_layers
+            + layer_moe_params * self.moe_layers
+            + layer_mlp_params * (self.num_layers - self.moe_layers)
+        )
+        total_activations = (q_activations + k_activations + v_activations) * batch
+
+        ab, wb = axwy_to_bytes(axwy)
+        total_bytes = total_params * wb + total_activations * ab
+        return total_bytes / 1e9
+
+
+class llama_nemotron:
+    pass
+
+
+class deepseek_v3:
+    pass
+
+
+def test_tops():
+    # llama405b
+    hf_repo = "mlx-community/Meta-Llama-3.1-405B-4bit"
+    model = llama(hf_repo)
+    print(hf_repo)
+    print("Prefill TOPS: {:.2f}".format(model.calc_inference_tops(1024, 0)))
+    print("Decode  TOPS: {:.2f}".format(model.calc_inference_tops(1, 1024)))
+    print("Prefill GBs:  {:.2f}".format(model.calc_inference_dram_gbs(1024, 0)))
+    print("Decode  GBs:  {:.2f}".format(model.calc_inference_dram_gbs(1, 1024)))
+
+    # llama405b-proxy
+    hf_repo = "mlx-community/Meta-Llama-3.1-405B-4bit"
+    shrink_conf = {"num_hidden_layers": 16}
+    model = llama(hf_repo, shrink_conf)
+    print(hf_repo + "-proxy")
+    print("Prefill TOPS: {:.2f}".format(model.calc_inference_tops(1024, 0)))
+    print("Decode  TOPS: {:.2f}".format(model.calc_inference_tops(1, 1024)))
+    print("Prefill GBs:  {:.2f}".format(model.calc_inference_dram_gbs(1024, 0)))
+    print("Decode  GBs:  {:.2f}".format(model.calc_inference_dram_gbs(1, 1024)))
+
+    # llama4-scout
+    hf_repo = "mlx-community/Llama-4-Scout-17B-16E-Instruct-4bit"
+    model = llama4(hf_repo)
+    print(hf_repo)
+    print("Prefill TOPS: {:.2f}".format(model.calc_inference_tops(1024, 0)))
+    print("Decode  TOPS: {:.2f}".format(model.calc_inference_tops(1, 1024)))
+    print("Prefill GBs:  {:.2f}".format(model.calc_inference_dram_gbs(1024, 0)))
+    print("Decode  GBs:  {:.2f}".format(model.calc_inference_dram_gbs(1, 1024)))
+
+if __name__ == "__main__":
+    test_tops()
---
+++ readme.md
@@ -0,0 +1,5 @@
+## A simple calculator of math/dram complexity of llms.
+
+```python
+python llm_calcer.py
+```

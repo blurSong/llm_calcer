7f20437b222f70f9250f9739da5cc59b3a0554d3
Tao Song <tasong@nvidia.com>
2025-05-15 17:59:00 +0800

    rename verbose results 3

--- llm_calcer.py
+++ llm_calcer.py
@@ -528,7 +528,7 @@ def calc_inference_complexity(
 
     table = [header, pvalues, dvaules]
     if verbose:
-        name = f"results/{model.name}_{axwy}_in{prompt}_out{output}_batch{batch}.csv"
+        name = f"results/{model.name}_{axwy}_in{prompt}_out{output}_b{batch}.csv"
         with open(name, "w", newline="") as f:
             writer = csv.writer(f)
             writer.writerows(table)
